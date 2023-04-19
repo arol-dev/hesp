23f612f716076edda9d3f541037db93a7d73c599
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-04-19 09:47:27 +0200

    added key for component that was breaking build

--- apps/hesp/src/components/PDform.tsx
+++ apps/hesp/src/components/PDform.tsx
@@ -1,17 +1,14 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function PDForm() {
-
-  const [progress, setProgress] = useState(0)
-
+  const [progress, setProgress] = useState(0);
 
   function handleClick(number: number) {
-    setProgress(number)
+    setProgress(number);
   }
 
   return (
     <div>
-
       {/* //////////////////////////////////
         Trust
          ///////////////////////////////// */}
@@ -19,12 +16,21 @@ function PDForm() {
       <div className="space-y-10 divide-y divide-gray-900/10 pl-5 pr-5 pb-10">
         <div className="grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3">
           <div className="px-4 sm:px-0">
-            <h2 className="text-base font-semibold leading-7 text-gray-900">Trust</h2>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Trust
+            </h2>
             <p className="mt-1 text-sm leading-6 text-gray-600 pb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.           </p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.{" "}
+            </p>
             <div className="h-2 relative max-w-screen-md mx-auto">
               {[...Array(10)].map((_, index) => (
-                <div className="h-1/6 w-full bg-gray-300 rounded-full">
+                <div
+                  className="h-1/6 w-full bg-gray-300 rounded-full"
+                  key={index}
+                >
                   <div
                     className=" h-full bg-gradient-to-r from-green-400 to-green-600 rounded-full"
                     style={{ width: `${(progress / 9) * 100}%` }}
@@ -32,18 +38,19 @@ function PDForm() {
                 </div>
               ))}
             </div>
-
           </div>
           <form className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
             <div className="px-4 py-6 sm:p-8">
               <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                 <div className="sm:col-span-4">
-                  <label htmlFor="website" className="block text-sm font-medium leading-6 text-gray-900">
+                  <label
+                    htmlFor="website"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
                     Satisfaction level
                   </label>
                   <div className="mt-2">
                     <div className="flex rounded-md  focus-within:ring-indigo-600 sm:max-w-md">
-
                       {/* <span className="hidden sm:block">
                       <button onClick={() => { handleClick(1) }} type="button" className=" mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                         1
@@ -80,10 +87,11 @@ function PDForm() {
                             key={number}
                             onClick={() => handleClick(number)}
                             type="button"
-                            className={`mr-2 inline-flex items-center rounded-md ${progress === number
-                              ? "bg-blue-500 text-white"
-                              : "bg-white text-gray-900"
-                              } px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50`}
+                            className={`mr-2 inline-flex items-center rounded-md ${
+                              progress === number
+                                ? "bg-blue-500 text-white"
+                                : "bg-white text-gray-900"
+                            } px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50`}
                           >
                             {number}
                           </button>
@@ -93,7 +101,10 @@ function PDForm() {
                   </div>
                 </div>
                 <div className="col-span-full">
-                  <label htmlFor="feel" className="block text-sm font-medium leading-6 text-gray-900">
+                  <label
+                    htmlFor="feel"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
                     What makes you feel this way?
                   </label>
                   <div className="mt-2">
@@ -102,24 +113,32 @@ function PDForm() {
                       name="feel"
                       rows={3}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      defaultValue={''}
+                      defaultValue={""}
                     />
                   </div>
-                  <p className="mt-3 text-sm leading-6 text-gray-400">Brief description for your profile. URLs are hyperlinked.</p>
+                  <p className="mt-3 text-sm leading-6 text-gray-400">
+                    Brief description for your profile. URLs are hyperlinked.
+                  </p>
                 </div>
                 <div className="col-span-full">
-                  <label htmlFor="improve" className="block text-sm font-medium leading-6 text-gray-900">
-                    What can you do to improve your satisfaction level?                  </label>
+                  <label
+                    htmlFor="improve"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    What can you do to improve your satisfaction level?{" "}
+                  </label>
                   <div className="mt-2">
                     <textarea
                       id="improve"
                       name="improve"
                       rows={3}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      defaultValue={''}
+                      defaultValue={""}
                     />
                   </div>
-                  <p className="mt-3 text-sm leading-6 text-gray-400">Brief description for your profile. URLs are hyperlinked.</p>
+                  <p className="mt-3 text-sm leading-6 text-gray-400">
+                    Brief description for your profile. URLs are hyperlinked.
+                  </p>
                 </div>
               </div>
             </div>
@@ -137,58 +156,88 @@ function PDForm() {
           </form>
         </div>
 
-
-
-
-
-
-
         {/* //////////////////////////////////
          WILL FOLLOW
          ///////////////////////////////// */}
 
-
-
         <div className="grid grid-cols-1 gap-x-8 gap-y-8 pt-10 md:grid-cols-3">
           <div className="px-4 sm:px-0">
-            <h2 className="text-base font-semibold leading-7 text-gray-900">Will Follow</h2>
-            <p className="mt-1 text-sm leading-6 text-gray-600">              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.           </p>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Will Follow
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              {" "}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.{" "}
+            </p>
           </div>
           <form className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
             <div className="px-4 py-6 sm:p-8">
               <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                 <div className="sm:col-span-4">
-                  <label htmlFor="website" className="block text-sm font-medium leading-6 text-gray-900">
+                  <label
+                    htmlFor="website"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
                     Satisfaction level
                   </label>
                   <div className="mt-2">
                     <div className="flex rounded-md  focus-within:ring-indigo-600 sm:max-w-md">
                       <span className="hidden sm:block">
-                        <button type="button" className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        <button
+                          type="button"
+                          className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        >
                           1
                         </button>
-                        <button type="button" className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        <button
+                          type="button"
+                          className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        >
                           2
                         </button>
-                        <button type="button" className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        <button
+                          type="button"
+                          className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        >
                           3
                         </button>
-                        <button type="button" className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        <button
+                          type="button"
+                          className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        >
                           4
                         </button>
-                        <button type="button" className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        <button
+                          type="button"
+                          className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        >
                           5
                         </button>
-                        <button type="button" className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        <button
+                          type="button"
+                          className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        >
                           6
                         </button>
-                        <button type="button" className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        <button
+                          type="button"
+                          className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        >
                           7
                         </button>
-                        <button type="button" className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        <button
+                          type="button"
+                          className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        >
                           8
                         </button>
-                        <button type="button" className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        <button
+                          type="button"
+                          className="mr-2 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        >
                           9
                         </button>
                       </span>
@@ -196,7 +245,10 @@ function PDForm() {
                   </div>
                 </div>
                 <div className="col-span-full">
-                  <label htmlFor="feel" className="block text-sm font-medium leading-6 text-gray-900">
+                  <label
+                    htmlFor="feel"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
                     What makes you feel this way?
                   </label>
                   <div className="mt-2">
@@ -205,24 +257,32 @@ function PDForm() {
                       name="feel"
                       rows={3}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      defaultValue={''}
+                      defaultValue={""}
                     />
                   </div>
-                  <p className="mt-3 text-sm leading-6 text-gray-400">Brief description for your profile. URLs are hyperlinked.</p>
+                  <p className="mt-3 text-sm leading-6 text-gray-400">
+                    Brief description for your profile. URLs are hyperlinked.
+                  </p>
                 </div>
                 <div className="col-span-full">
-                  <label htmlFor="improve" className="block text-sm font-medium leading-6 text-gray-900">
-                    What can you do to improve your satisfaction level?                  </label>
+                  <label
+                    htmlFor="improve"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    What can you do to improve your satisfaction level?{" "}
+                  </label>
                   <div className="mt-2">
                     <textarea
                       id="improve"
                       name="improve"
                       rows={3}
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      defaultValue={''}
+                      defaultValue={""}
                     />
                   </div>
-                  <p className="mt-3 text-sm leading-6 text-gray-400">Brief description for your profile. URLs are hyperlinked.</p>
+                  <p className="mt-3 text-sm leading-6 text-gray-400">
+                    Brief description for your profile. URLs are hyperlinked.
+                  </p>
                 </div>
               </div>
             </div>
@@ -241,7 +301,7 @@ function PDForm() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default PDForm
+export default PDForm;
