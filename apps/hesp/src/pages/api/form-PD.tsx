c309704b556121126bd053dcaff12ce22ff722a8
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-04-21 13:53:47 +0200

    add types

--- apps/hesp/src/pages/api/form-PD.tsx
+++ apps/hesp/src/pages/api/form-PD.tsx
@@ -1,34 +1,9 @@
-// import { NextApiRequest, NextApiResponse } from "next"
-
-// export default function handler(req: NextApiRequest, res: NextApiResponse) {
-//   // Get data submitted in request's body.
-//   const body = req.body
-
-//   // Optional logging to see the responses
-//   // in the command line where next.js app is running.
-//   console.log('body from api: ', body)
-
-//   // Guard clause checks for first and last name,
-//   // and returns early if they are not found
-//   if (!body) {
-//     // Sends a HTTP bad request error code
-//     return res.status(400).json({ data: 'First or last name not found' })
-//   }
-
-
-
-//   // Found the name.
-//   // Sends a HTTP success code
-//   res.status(200).json({ trust: body.trust })
-// }
-
-
-
 import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from "next"
 
 const prisma = new PrismaClient()
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const body = req.body
     const checkpoint = await prisma.pDCcheckpoint.create({
