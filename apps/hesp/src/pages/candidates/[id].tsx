4a1c6113dab2bb159b1b46ee70c593f6adee9fcf
Manel Soler <manelsoler@gmail.com>
2023-05-04 12:59:29 +0200

    edit profile page

--- apps/hesp/src/pages/candidates/[id].tsx
+++ apps/hesp/src/pages/candidates/[id].tsx
@@ -2,31 +2,55 @@ import Candidate from "@/components/Candidate";
 import { GetServerSideProps } from "next";
 
 export function Profile({ person, WOLs, PDs }: any) {
-  return <Candidate WOLs={WOLs} person={person} pds={PDs}></Candidate>;
+
+  const updatePerson = (updatedPersonData: any) => {
+    
+    fetch(`http://localhost:3000/api/he/${updatedPersonData.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedPersonData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+   
+      });
+  
+    return <Candidate
+      WOLs={WOLs}
+      person={person}
+      pds={PDs}
+      updatePerson={updatePerson}
+    ></Candidate>
+    
+    
+  }
 }
-export default Candidate;
+  export default Candidate;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { id }: any = context.query
-  const domainName = context.req.headers.host;
-  const person = await fetch(`http://${domainName}/api/he/${id}`).then((res) =>
-    res.json()
-  );
+  export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { id }: any = context.query
+    const domainName = context.req.headers.host;
+    const person = await fetch(`http://${domainName}/api/he/${id}`).then((res) =>
+      res.json()
+    );
 
-  const WOLs = await fetch(`http://${domainName}/api/wol/${id}`).then((res) =>
-    res.json()
-  );
+    const WOLs = await fetch(`http://${domainName}/api/wol/${id}`).then((res) =>
+      res.json()
+    );
 
-  const PDs = await fetch(`http://${domainName}/api/pdc/${id}`).then((res) =>
-    res.json()
-  );
+    const PDs = await fetch(`http://${domainName}/api/pdc/${id}`).then((res) =>
+      res.json()
+    );
 
 
-  return {
-    props: {
-      person,
-      WOLs,
-      PDs
-    },
-  };
-};
+    return {
+      props: {
+        person,
+        WOLs,
+        PDs
+      },
+    };
+  }
+
