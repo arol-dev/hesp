6ba073595a090dc4710796034f8b9cf1e7f64a98
Imagine96 <rubged9606@gmail.com>
2023-05-06 11:39:31 +0200

    test(apps/hesp): basic test setup for automatic trigger from github workflow to vercel preview

--- apps/hesp/cypress/e2e/login.cy.js
+++ apps/hesp/cypress/e2e/login.cy.js
@@ -11,34 +11,24 @@
 // please read our getting started guide:
 // https://on.cypress.io/introduction-to-cypress
 
-describe('example to-do app', () => {
+let preview_URL = Cypress.env('host')
+if(!preview_URL) throw new Error('host env not found')
 
+describe('example to-do app', () => {
+  
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit('https://example.cypress.io/todo')
+    cy.visit(preview_URL)
   })
 
-  it('displays two todo items by default', () => {
-    if (Cypress.env('host') !== 'https://google.com') {
-      throw new Error('VERCEL_PREVIEW_OUTPUT is not set')
-    }
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
-    cy.get('.todo-list li').should('have.length', 2)
-
-    // We can go even further and check that the default todos each contain
-    // the correct text. We use the `first` and `last` functions
-    // to get just the first and last matched elements individually,
-    // and then perform an assertion with `should`.
-    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+  it('works', () => {
+    cy.get('form').should('exist')
   })
 
-  it('can add new todo items', () => {
+ /*  it('can add new todo items', () => {
     // We'll store our item text in a variable so we can reuse it
     const newItem = 'Feed the cat'
 
@@ -143,5 +133,5 @@ describe('example to-do app', () => {
       // Finally, make sure that the clear button no longer exists.
       cy.contains('Clear completed').should('not.exist')
     })
-  })
+  }) */
 })
