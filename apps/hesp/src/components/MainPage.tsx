86c2bd575281779971f7040252a5dfbeb9f51982
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-05-10 15:47:26 +0200

    delete commented out code + fix condition

--- apps/hesp/src/components/MainPage.tsx
+++ apps/hesp/src/components/MainPage.tsx
@@ -33,7 +33,7 @@ const List: React.FC<props> = ({ user, jwt, Trainees }) => {
     }
   }
 
-
+  console.log('data', dataToMap)
 
 
   return (
@@ -53,7 +53,7 @@ const List: React.FC<props> = ({ user, jwt, Trainees }) => {
           </div>
         </div>
         <div className="mt-8 flow-root">
-          {matchingUser && matchingUser.length > 0 ? (
+          {matchingUser && matchingUser.length > 0 && dataToMap.length > 0 ? (
             <div className="px-4 sm:px-6 lg:px-8 pb-12">
               <div className="mt-8 flow-root">
                 <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -129,81 +129,7 @@ const List: React.FC<props> = ({ user, jwt, Trainees }) => {
                 </div>
               </div>
             </div>
-
-
-
-
-            // <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            //   <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-            //     <table className="w-full divide-y divide-gray-300">
-            //       <thead>
-            //         <tr>
-            //           <th
-            //             scope="col"
-            //             className="py-3.5 pl-4 pr-80 text-left text-sm font-semibold text-gray-900 sm:pl-0"
-            //           >
-            //             NAME
-            //           </th>
-            //           <th
-            //             scope="col"
-            //             className="px-3 py-3.5 text-center text-sm font-semibold text-gray-900"
-            //           >
-            //             LAST CHECKPOINT
-            //           </th>
-            //           <th scope="col" className=" py-3.5 pl-3 pr-4 sm:pr-0">
-            //             <span className="sr-only">Edit</span>
-            //           </th>
-            //           <th scope="col" className="py-3.5 pl-3 pr-4 sm:pr-0">
-            //             <span className="sr-only">New Checkpoint</span>
-            //           </th>
-            //         </tr>
-            //       </thead>
-            //       <tbody className="divide-y divide-gray-200">
-            //         {dataToMap.map((person: any, index: number) => (
-            //           <tr key={index}>
-            //             <td className="whitespace-nowrap py-4 pl-4 pr-80 text-sm font-medium text-gray-900 sm:pl-0">
-            //               <Link
-            //                 href={`/candidates/${person.id}`}
-            //                 className="text-indigo-600 hover:text-indigo-900 "
-            //               >
-            //                 {person.firstName + " " + person.lastName}
-            //                 <p className="whitespace-nowrap  text-sm text-gray-500">
-            //                   {person.email}
-            //                 </p>
-            //               </Link>
-            //             </td>
-            //             <td className="text-center px-2 py-4 text-sm text-gray-500">
-            //               {lastCheckpoint(person)}
-            //             </td>
-            //             <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-            //               <a
-            //                 href="#"
-            //                 className="text-indigo-600 hover:text-indigo-900"
-            //               >
-            //                 Edit
-            //                 <span className="sr-only">
-            //                   , {person.firstName + " " + person.lastName}
-            //                 </span>
-            //               </a>
-            //             </td>
-            //             <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-            //               <a
-            //                 href={`/candidates/${person.id}/checkpoint`}
-            //                 className="text-indigo-600 hover:text-indigo-900"
-            //               >
-            //                 New Checkpoint
-            //                 <span className="sr-only">
-            //                   , {person.firstName + " " + person.lastName}
-            //                 </span>
-            //               </a>
-            //             </td>
-            //           </tr>
-            //         ))}
-            //       </tbody>
-            //     </table>
-            //   </div>
-            // </div>
-          ) : (
+          ) :
             <div className="px-4 py-5 sm:px-6">
               <h3 className="text-lg leading-6 font-medium text-gray-900">
                 No candidates
@@ -212,7 +138,7 @@ const List: React.FC<props> = ({ user, jwt, Trainees }) => {
                 You currently don't have any candidates assigned to you.
               </p>
             </div>
-          )
+
           }
         </div >
       </div >
