045c7db60699c296bf4835de45eb590981bbb9fb
Margarita Varetckaia <mvaretckaia@gmail.com>
2023-05-12 10:06:54 +0200

    fix

--- apps/hesp/src/components/MainPage.tsx
+++ apps/hesp/src/components/MainPage.tsx
@@ -26,7 +26,7 @@ const List: React.FC<props> = ({ user, jwt, Trainees }) => {
 
     const pdc = person.PDCcheckpoint;
 
-    if (pdc !== undefined) {
+    if (pdc !== undefined && pdc.length > 0) {
       const last = pdc[pdc.length - 1];
       const ago = moment(last.createdAt).startOf("day").fromNow();
       return ago;
