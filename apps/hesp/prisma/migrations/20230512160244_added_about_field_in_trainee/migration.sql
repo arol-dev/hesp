9aa00eceabd58d88297e9184c92340e2798e9128
Manel Soler <manelsoler@gmail.com>
2023-05-13 13:28:56 +0200

    fixins migrations table

--- apps/hesp/prisma/migrations/20230512160244_added_about_field_in_trainee/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Trainee" ADD COLUMN     "about" TEXT;
--- apps/hesp/prisma/migrations/20230513091521_removing_duplicate_about_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `about` on the `Trainee` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Trainee" DROP COLUMN "about";
