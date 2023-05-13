d54d4889dd81fe4725e3d7a20790f84354f9eea0
Manel Soler <manelsoler@gmail.com>
2023-05-13 12:29:17 +0200

    fix prisma migration and about field edit mode

---
+++ apps/hesp/prisma/migrations/20230513091521_removing_duplicate_about_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `about` on the `Trainee` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Trainee" DROP COLUMN "about";
