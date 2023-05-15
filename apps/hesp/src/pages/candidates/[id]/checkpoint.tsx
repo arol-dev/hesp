1e02ae909caf88f7175b46706c04190043cd0ae8
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-05-15 15:27:08 +0200

    fix, added types

--- apps/hesp/src/pages/candidates/[id]/checkpoint.tsx
+++ apps/hesp/src/pages/candidates/[id]/checkpoint.tsx
@@ -1,75 +1,89 @@
-import Checkpoint from "@/components/Checkpoints/NewCheckpoint"
-import { GetServerSideProps } from "next"
-import { IWOLcheckpoint, IPDCcheckpoint } from "../../../../types"
-import { PrismaClient } from "@prisma/client"
+import Checkpoint from '@/components/Checkpoints/NewCheckpoint';
+import { GetServerSideProps } from 'next';
+import { IWOLcheckpoint, IPDCcheckpoint } from '../../../../types';
+import { PrismaClient } from '@prisma/client';
+import { authenticateAndGetToken } from '../../../../lib/auth/authUtils';
 
 type INewCheckpointProps = {
-  id: number
-  lastPDCheckpoint: IPDCcheckpoint
-  lastWOLCheckpoint: IWOLcheckpoint
-}
-const prisma = new PrismaClient()
+  id: number;
+  lastPDCheckpoint: IPDCcheckpoint;
+  lastWOLCheckpoint: IWOLcheckpoint;
+};
+const prisma = new PrismaClient();
 
-function NewCheckpoint({ id, lastPDCheckpoint, lastWOLCheckpoint }: INewCheckpointProps) {
+function NewCheckpoint({
+  id,
+  lastPDCheckpoint,
+  lastWOLCheckpoint,
+}: INewCheckpointProps) {
   return (
-    <Checkpoint id={id} lastPDCheckpoint={lastPDCheckpoint} lastWOLCheckpoint={lastWOLCheckpoint} ></Checkpoint>
-  )
+    <Checkpoint
+      id={id}
+      lastPDCheckpoint={lastPDCheckpoint}
+      lastWOLCheckpoint={lastWOLCheckpoint}
+    ></Checkpoint>
+  );
 }
-export default NewCheckpoint
+export default NewCheckpoint;
 
-
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
   const { id }: any = context.query;
+  const decodedToken = await authenticateAndGetToken(context);
+  const cookies = context.req.headers.cookie;
 
+  if (!cookies || !decodedToken) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
 
-  const oneMonthAgo = new Date()
-  oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1)
-
-  const allPDs = await prisma.pDCcheckpoint.findMany({
-    where: {
-      traineeId: parseInt(id)
-    }
-  })
-
-  const allWols = await prisma.wOLcheckpoint.findMany({
-    where: {
-      traineeId: parseInt(id)
-    }
-  })
+  const oneMonthAgo = new Date();
+  oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
 
   const lastPDCheckpoint = await prisma.pDCcheckpoint.findFirst({
     where: {
       traineeId: parseInt(id),
       createdAt: {
-        gte: oneMonthAgo
+        gte: oneMonthAgo,
       },
     },
     include: {
-      SessionNotes: true
+      SessionNotes: true,
     },
     orderBy: {
-      createdAt: 'desc'
-    }
-  })
-
+      createdAt: 'desc',
+    },
+  });
 
   const lastWOLCheckpoint = await prisma.wOLcheckpoint.findFirst({
     where: {
       traineeId: parseInt(id),
       createdAt: {
-        gte: oneMonthAgo
-      }
+        gte: oneMonthAgo,
+      },
     },
     orderBy: {
-      createdAt: 'desc'
-    }
-  })
+      createdAt: 'desc',
+    },
+  });
   return {
     props: {
       id,
-      lastPDCheckpoint: lastPDCheckpoint ? { ...lastPDCheckpoint, createdAt: lastPDCheckpoint.createdAt.toISOString() } : null,
-      lastWOLCheckpoint: lastWOLCheckpoint ? { ...lastWOLCheckpoint, createdAt: lastWOLCheckpoint.createdAt.toISOString() } : null,
+      lastPDCheckpoint: lastPDCheckpoint
+        ? {
+            ...lastPDCheckpoint,
+            createdAt: lastPDCheckpoint.createdAt.toISOString(),
+          }
+        : null,
+      lastWOLCheckpoint: lastWOLCheckpoint
+        ? {
+            ...lastWOLCheckpoint,
+            createdAt: lastWOLCheckpoint.createdAt.toISOString(),
+          }
+        : null,
     },
   };
 };
