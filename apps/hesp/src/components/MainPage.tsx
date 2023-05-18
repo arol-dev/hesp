54a2473da5c7513a60cac499a8f41fbb40c8e9fd
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-05-18 13:23:15 +0200

    feat, updated hes to be able to be added from the index page

--- apps/hesp/src/components/MainPage.tsx
+++ apps/hesp/src/components/MainPage.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { ITrainee, IUser } from "../../types";
 import moment from "moment";
 import Navbar from "./Navbar";
+import { useState } from "react";
+import AddCoach from "./Team/FormAddCoach";
 
 interface props {
   user: IUser[];
@@ -14,6 +16,7 @@ interface props {
 
 const List: React.FC<props> = ({ user, jwt, Trainees }) => {
   const showIHe = jwt.role === "STAFF";
+  const [showAddCandidateForm, setShowAddCandidateForm] = useState(false);
 
   const matchingUser = user.filter((user: IUser) => {
     console.log(user.id);
@@ -44,13 +47,17 @@ const List: React.FC<props> = ({ user, jwt, Trainees }) => {
           <div className="mt-4 pr-8 sm:ml-16 sm:mt-0 sm:flex-none">
             <button
               type="button"
-              onClick={() => console.log(matchingUser)}
+              onClick={() => setShowAddCandidateForm(true)}
               className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-medium text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               + Add new candidate
             </button>
           </div>
         </div>
+        <AddCoach
+          showForm={showAddCandidateForm}
+          closeForm={() => setShowAddCandidateForm(false)}
+        />
         <div className="mt-8 flow-root">
           {matchingUser && matchingUser.length > 0 && dataToMap.length > 0 ? (
             <div className="px-4 sm:px-6 lg:px-8 pb-12">
@@ -61,27 +68,36 @@ const List: React.FC<props> = ({ user, jwt, Trainees }) => {
                       <table className="min-w-full divide-y divide-gray-300">
                         <thead className="bg-gray-50">
                           <tr>
-                            <th scope="col" className="py-3.5 pl-4 pr-80 text-left text-sm font-medium text-gray-500 sm:pl-6">
+                            <th
+                              scope="col"
+                              className="py-3.5 pl-4 pr-80 text-left text-sm font-medium text-gray-500 sm:pl-6"
+                            >
                               NAME
                             </th>
-                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-medium text-gray-500">
+                            <th
+                              scope="col"
+                              className="px-3 py-3.5 text-left text-sm font-medium text-gray-500"
+                            >
                               LAST CHECKPOINT
                             </th>
 
-                            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                            <th
+                              scope="col"
+                              className="relative py-3.5 pl-3 pr-4 sm:pr-6"
+                            >
                               <span className="sr-only">Edit</span>
                             </th>
-                            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                            <th
+                              scope="col"
+                              className="relative py-3.5 pl-3 pr-4 sm:pr-6"
+                            >
                               <span className="sr-only">New Checkpoint</span>
                             </th>
                           </tr>
                         </thead>
                         <tbody className="divide-y divide-gray-200 bg-white">
-                           {Trainees.map((person: ITrainee, index: number) => (
+                          {Trainees.map((person: ITrainee, index: number) => (
                             <tr data-cy="trainee-row" key={index}>
-
- 
- 
                               <td className="flex whitespace-nowrap py-4 pl-4  pr-80 text-sm font-medium text-gray-900 sm:pl-6">
                                 <div className="mr-3">
                                   <img
@@ -134,7 +150,7 @@ const List: React.FC<props> = ({ user, jwt, Trainees }) => {
                 </div>
               </div>
             </div>
-          ) :
+          ) : (
             <div className="px-4 py-5 sm:px-6">
               <h3 className="text-lg leading-6 font-medium text-gray-900">
                 No candidates
@@ -143,10 +159,9 @@ const List: React.FC<props> = ({ user, jwt, Trainees }) => {
                 You currently don't have any candidates assigned to you.
               </p>
             </div>
-
-          }
-        </div >
-      </div >
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- apps/hesp/src/components/Team/FormAddCoach.tsx
+++ apps/hesp/src/components/Team/FormAddCoach.tsx
@@ -1,8 +1,17 @@
-import { useState } from 'react';
+import { useState } from "react";
+import Teampage from "./Teampage";
 
 interface AddCoachProps {
   closeForm: () => void;
   showForm: boolean;
+}
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+  role?: string;
+  email: string;
+  password?: string;
 }
 
 function AddCoach({ closeForm, showForm }: AddCoachProps) {
@@ -10,26 +19,36 @@ function AddCoach({ closeForm, showForm }: AddCoachProps) {
     return null;
   }
 
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    role: 'STAFF',
-    email: '',
-    password: '12345',
-  });
+  // get the pathname of the current page
+  const path = window.location.pathname;
+  const teamPage = path.includes("team");
+  const formFields = teamPage
+    ? {
+        firstName: "",
+        lastName: "",
+        role: "STAFF",
+        email: "",
+        password: "12345",
+      }
+    : {
+        firstName: "",
+        lastName: "",
+        email: "",
+      };
+  const [formData, setFormData] = useState<FormData>(formFields);
 
-  function handleInputChange(event: any) {
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   }
 
-  async function handleSubmit(event: any) {
-    event.preventDefault();
+  async function handleSubmit() {
+    const endPoint = teamPage ? "/api/staff/createCoach" : "/api/he/he";
 
-    const response = await fetch('/api/staff/createCoach', {
-      method: 'POST',
+    const response = await fetch(endPoint, {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
     });
@@ -40,87 +59,87 @@ function AddCoach({ closeForm, showForm }: AddCoachProps) {
       closeForm();
       window.location.reload();
     } else {
-      console.error('Error submitting form:', result);
+      console.error("Error submitting form:", result);
     }
   }
 
   return (
     <form>
-      <div className='fixed z-10 inset-0 overflow-y-auto'>
-        <div className='flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center'>
-          <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity'></div>
-          <div className='bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all w-full max-w-xl'>
-            <div className='space-y-12 px-6 py-4'>
-              <div className='grid grid-cols-1 gap-x-8 gap-y-10'>
-                <div className='grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
-                  <div className='sm:col-span-3'>
+      <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center">
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+          <div className="bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all w-full max-w-xl">
+            <div className="space-y-12 px-6 py-4">
+              <div className="grid grid-cols-1 gap-x-8 gap-y-10">
+                <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                  <div className="sm:col-span-3">
                     <label
-                      htmlFor='first-name'
-                      className='block text-sm font-medium leading-6 text-gray-900'
+                      htmlFor="first-name"
+                      className="block text-sm font-medium leading-6 text-gray-900"
                     >
                       Name
                     </label>
-                    <div className='mt-2'>
+                    <div className="mt-2">
                       <input
-                        type='text'
-                        name='firstName'
+                        type="text"
+                        name="firstName"
                         onChange={handleInputChange}
-                        id='first-name'
-                        autoComplete='given-name'
-                        className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                        id="first-name"
+                        autoComplete="given-name"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
                     </div>
                   </div>
 
-                  <div className='sm:col-span-3'>
+                  <div className="sm:col-span-3">
                     <label
-                      htmlFor='last-name'
-                      className='block text-sm font-medium leading-6 text-gray-900'
+                      htmlFor="last-name"
+                      className="block text-sm font-medium leading-6 text-gray-900"
                     >
                       Surname
                     </label>
-                    <div className='mt-2'>
+                    <div className="mt-2">
                       <input
-                        type='text'
-                        name='lastName'
-                        id='last-name'
+                        type="text"
+                        name="lastName"
+                        id="last-name"
                         onChange={handleInputChange}
-                        autoComplete='family-name'
-                        className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                        autoComplete="family-name"
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       />
                     </div>
                   </div>
-                  <div className='sm:col-span-6'>
+                  <div className="sm:col-span-6">
                     <label
-                      htmlFor='email'
-                      className='block text-sm font-medium leading-6 text-gray-900'
+                      htmlFor="email"
+                      className="block text-sm font-medium leading-6 text-gray-900"
                     >
                       Email
                     </label>
-                    <div className='mt-2'>
-                      <div className='relative'>
+                    <div className="mt-2">
+                      <div className="relative">
                         <svg
-                          xmlns='http://www.w3.org/2000/svg'
-                          fill='none'
-                          viewBox='0 0 24 24'
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
                           strokeWidth={1.5}
-                          stroke='currentColor'
-                          className='absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5'
+                          stroke="currentColor"
+                          className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5"
                         >
                           <path
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            d='M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75'
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
                           />
                         </svg>
                         <input
-                          id='email'
-                          name='email'
+                          id="email"
+                          name="email"
                           onChange={handleInputChange}
-                          type='email'
-                          autoComplete='email'
-                          className='block w-full pl-8 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                          placeholder='frodo.baggins@thefellowship.com'
+                          type="email"
+                          autoComplete="email"
+                          className="block w-full pl-8 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          placeholder="frodo.baggins@thefellowship.com"
                         />
                       </div>
                     </div>
@@ -128,18 +147,18 @@ function AddCoach({ closeForm, showForm }: AddCoachProps) {
                 </div>
               </div>
             </div>
-            <div className='mt-6 px-6 py-4 flex items-center justify-end gap-x-6'>
+            <div className="mt-6 px-6 py-4 flex items-center justify-end gap-x-6">
               <button
                 onClick={closeForm}
-                type='button'
-                className='text-sm font-semibold leading-6 text-gray'
+                type="button"
+                className="text-sm font-semibold leading-6 text-gray"
               >
                 Cancel
               </button>
               <button
                 onClick={handleSubmit}
-                type='submit'
-                className='rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                type="submit"
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Save
               </button>
