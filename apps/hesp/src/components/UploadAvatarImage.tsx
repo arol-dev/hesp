13301c40b362b153fa0e4f86d3b73b12131f250a
Manel Soler <manelsoler@gmail.com>
2023-06-01 09:51:16 +0200

    add try-catch

--- apps/hesp/src/components/UploadAvatarImage.tsx
+++ apps/hesp/src/components/UploadAvatarImage.tsx
@@ -1,36 +1,43 @@
-import { createClient } from '@supabase/supabase-js';
-import {v4 as uuidv4} from 'uuid';
+import { createClient } from "@supabase/supabase-js";
+import { v4 as uuidv4 } from "uuid";
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-  throw new Error('Missing Supabase environment variables');
+let supabase: ReturnType<typeof createClient>;
+
+try {
+  if (
+    !process.env.NEXT_PUBLIC_SUPABASE_URL ||
+    !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  ) {
+    throw new Error("Missing Supabase environment variables");
+  }
+
+  supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+} catch (error) {
+  console.error(error);
 }
 
-const supabase = createClient (process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
 export async function uploadAvatarImage(file: File, id: string) {
-  
-  const bucketName = 'avatar images';
+  const bucketName = "avatar images";
   const filePath = `${bucketName}/${id}/${uuidv4()}`;
 
-
-  let { error: uploadError } = await supabase
-    .storage
+  let { error: uploadError } = await supabase.storage
     .from(bucketName)
-    .upload(filePath, file); 
+    .upload(filePath, file);
 
   if (uploadError) {
     throw uploadError;
   }
 
-  let { data: publicUrl } = supabase
-    .storage
+  let { data: publicUrl } = supabase.storage
     .from(bucketName)
     .getPublicUrl(filePath);
-  
+
   if (!publicUrl) {
-    throw new Error('Could not get public URL');
-  } 
+    throw new Error("Could not get public URL");
+  }
 
-  return publicUrl
+  return publicUrl;
 }
-
