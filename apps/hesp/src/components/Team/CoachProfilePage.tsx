0ca1cc97d9d4d01a6444949c1d143b11416dc69f
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-06-02 19:21:13 +0200

    feat, updated conditional editing of a coach, relative to if it is your profile or if you are an admin

--- apps/hesp/src/components/Team/CoachProfilePage.tsx
+++ apps/hesp/src/components/Team/CoachProfilePage.tsx
@@ -5,10 +5,13 @@ import { useRouter } from "next/router";
 
 interface CoachProfilePageProps {
   person: IUser;
+  jwt: IUser;
 }
 
-function CoachProfilePage({ person }: CoachProfilePageProps) {
+function CoachProfilePage({ person, jwt }: CoachProfilePageProps) {
   const router = useRouter();
+
+  const isAllowedToEdit = jwt.role === "ADMIN" || jwt.id === person.id;
 
   const [selectedPicFile, setSelectedPicFile] = useState<File | null>(null);
   const [picpreview, setPicPreview] = useState<string | undefined>(
@@ -100,14 +103,18 @@ function CoachProfilePage({ person }: CoachProfilePageProps) {
                       Name
                     </label>
                     <div className="mt-2">
-                      <textarea
-                        onChange={(event) => handleInputChange(event)}
-                        id="firstName"
-                        name="firstName"
-                        rows={1}
-                        className="resize-none block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        defaultValue={person.firstName}
-                      />
+                      {isAllowedToEdit ? (
+                        <textarea
+                          onChange={(event) => handleInputChange(event)}
+                          id="firstName"
+                          name="firstName"
+                          rows={1}
+                          className="resize-none block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          defaultValue={person.firstName}
+                        />
+                      ) : (
+                        <p>{person.firstName}</p>
+                      )}
                     </div>
                   </div>
                   <div className="col-span-2">
@@ -118,14 +125,18 @@ function CoachProfilePage({ person }: CoachProfilePageProps) {
                       Surname
                     </label>
                     <div className="mt-2">
-                      <textarea
-                        onChange={(event) => handleInputChange(event)}
-                        id="lastName"
-                        name="lastName"
-                        rows={1}
-                        className="resize-none first-letter:block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        defaultValue={person.lastName}
-                      />
+                      {isAllowedToEdit ? (
+                        <textarea
+                          onChange={(event) => handleInputChange(event)}
+                          id="lastName"
+                          name="lastName"
+                          rows={1}
+                          className="resize-none first-letter:block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          defaultValue={person.lastName}
+                        />
+                      ) : (
+                        <p>{person.lastName}</p>
+                      )}
                     </div>
                   </div>
 
@@ -137,14 +148,18 @@ function CoachProfilePage({ person }: CoachProfilePageProps) {
                       Email
                     </label>
                     <div className="mt-2">
-                      <textarea
-                        onChange={(event) => handleInputChange(event)}
-                        id="email"
-                        name="email"
-                        rows={1}
-                        className=" resize-none block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        defaultValue={person.email}
-                      />
+                      {isAllowedToEdit ? (
+                        <textarea
+                          onChange={(event) => handleInputChange(event)}
+                          id="email"
+                          name="email"
+                          rows={1}
+                          className=" resize-none block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          defaultValue={person.email}
+                        />
+                      ) : (
+                        <p>{person.email}</p>
+                      )}
                     </div>
                   </div>
 
@@ -167,12 +182,14 @@ function CoachProfilePage({ person }: CoachProfilePageProps) {
                           className="h-12 w-12 rounded-full object-cover"
                         />
                       )}
-                      <label
-                        htmlFor="photoInput"
-                        className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                      >
-                        Change
-                      </label>
+                      {isAllowedToEdit && (
+                        <label
+                          htmlFor="photoInput"
+                          className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                        >
+                          Change
+                        </label>
+                      )}
                       <input
                         type="file"
                         id="photoInput"
@@ -185,21 +202,23 @@ function CoachProfilePage({ person }: CoachProfilePageProps) {
                   </div>
                 </div>
               </div>
-              <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
-                <button
-                  type="button"
-                  className="text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Cancel
-                </button>
-                <button
-                  onClick={() => handleSubmit(event)}
-                  type="submit"
-                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Save
-                </button>
-              </div>
+              {isAllowedToEdit && (
+                <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
+                  <button
+                    type="button"
+                    className="text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={() => handleSubmit(event)}
+                    type="submit"
+                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Save
+                  </button>
+                </div>
+              )}
             </form>
           </div>
         </div>
--- apps/hesp/src/pages/team/[id].tsx
+++ apps/hesp/src/pages/team/[id].tsx
@@ -1,30 +1,34 @@
 import CoachProfilePage from "@/components/Team/CoachProfilePage";
 import Navbar from "@/components/Navbar";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import { IUser } from "../../../types";
+import { AuthContext } from "../../../types";
 import { authenticateAndGetToken } from "../../../lib/auth/authUtils";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 interface CoachPageProps {
   person: IUser;
+  jwt: IUser;
 }
 
-function CoachPage({ person }: CoachPageProps) {
+function CoachPage({ person, jwt }: CoachPageProps) {
   return (
     <>
       <Navbar headerText="Profile"></Navbar>
-      <CoachProfilePage person={person}></CoachProfilePage>
+      <CoachProfilePage jwt={jwt} person={person}></CoachProfilePage>
     </>
   );
 }
 
 export default CoachPage;
 
-export const getServerSideProps: GetServerSideProps = async (context: any) => {
-  const decodedToken = await authenticateAndGetToken(context);
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const decodedToken = await authenticateAndGetToken(context as AuthContext);
   const cookies = context.req.headers.cookie;
-  const { id }: any = context.query;
+  const { id } = context.query;
 
   if (!cookies || !decodedToken || !id) {
     return {
@@ -34,6 +38,7 @@ export const getServerSideProps: GetServerSideProps = async (context: any) => {
       },
     };
   }
+  console.log(decodedToken);
 
   const person = await prisma.user.findUnique({
     where: {
@@ -44,6 +49,7 @@ export const getServerSideProps: GetServerSideProps = async (context: any) => {
   return {
     props: {
       person,
+      jwt: decodedToken,
     },
   };
 };
