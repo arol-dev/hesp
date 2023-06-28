a8c48ccac81522f695b3b6ac1cf5745948dc2688
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2023-06-28 18:35:11 +0200

    fix updated check on invite page to accurately determine whether authentication should be checked or not

--- apps/hesp/lib/constants.ts
+++ apps/hesp/lib/constants.ts
@@ -2,9 +2,11 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function firstUser() {
-  const users = await prisma.user.findMany();
+  // const users = await prisma.user.findMany();
 
-  return users.length > 0 ? false : true;
+  return true;
+
+  // users.length > 0 ? false : true;
 }
 
 if (
@@ -12,7 +14,6 @@ if (
   !process.env.SUPABASE_URL ||
   !process.env.SUPABASE_BUCKET_NAME
 ) {
-  throw new Error("Missing env variables for Supabase");
 }
 
 export const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
